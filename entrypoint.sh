97e6a3ae068c67f8be1750b819e889cc515a8d42
Raphael Rodrigues <noh4nsen@gmail.com>
2023-08-23 12:37:29 -0300

    transform output into base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,7 @@ while read -r project; do
     echo -e "--- Finished Report on $project ---\n"
 done < <(echo $projects | tr -d "'" | jq -r '.projects[]' )
 
+encoded_report=$(echo -n $report | base64)
+echo -n $encoded_report
 
-echo "tfsec_report=$(echo -n $report | base64)" >> $GITHUB_OUTPUT
+echo "tfsec_report=$(echo -n $encoded_report)" >> $GITHUB_OUTPUT
